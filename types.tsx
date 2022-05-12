03523a63f213e0eb18c862c9dba934a5b322c450
Choco02 <sugarzx01@gmail.com>
2022-05-12 18:47:28 -0300

    refactor: Params types

--- types.tsx
+++ types.tsx
@@ -15,7 +15,7 @@ declare global {
 
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
+  Modal: { itemId: string } | undefined;
   NotFound: undefined;
 };
 
@@ -27,6 +27,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 export type RootTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
+  Modal: { itemId: string, title: string, price: string } | undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
