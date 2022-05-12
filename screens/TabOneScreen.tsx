f070b74d7eca7ee043cc85af5709ac07a927a85c
Choco02 <sugarzx01@gmail.com>
2022-05-12 18:42:36 -0300

    feat: Main tab (Items tab)

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,67 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { FlatList, ListRenderItem, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { Text, View } from '../components/Themed';
+import ListItem from '../components/ListItem';
 import { RootTabScreenProps } from '../types';
+import useFormatPrice from '../hooks/useFormatPrice';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [amount, setAmount] = useState(300);
+  const [data, setData] = useState<Data[]>([]);
+
+  interface Data {
+    id: string;
+    title: string;
+    price: string;
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      AsyncStorage.getItem('items').then((items) => {
+        const dataStorage: Data[] = JSON.parse(items!) || [];
+        dataStorage.length > 0 && dataStorage.push(...dataStorage);
+        setData([...new Set(dataStorage)]);
+      });
+      AsyncStorage.getItem('cash').then((cash) => {
+        cash && setAmount(Number(cash));
+      });
+    }, []),
+  );
+
+  const renderItem: ListRenderItem<Data> = ({ item }) => (
+    <ListItem
+      item={item}
+      amount={amount}
+      setAmount={setAmount}
+      navigation={navigation}
+    />
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>
+        Dinheiro disponível
+        {' R$ '}
+        {useFormatPrice(amount)}
+      </Text>
+      <View style={styles.separator} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
+      {
+        data.length !== 0
+          ? <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
+          : (
+            <Text style={{ fontSize: 25 }}>
+              {' '}
+              Clique em
+              {' '}
+              <FontAwesome name='plus' size={20} color='white' />
+              {' '}
+              para adicionar
+            </Text>
+          )
+      }
     </View>
   );
 }
@@ -21,8 +73,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: 'bold',
+    justifyContent: 'space-between',
+    // textAlign: 'center',
   },
   separator: {
     marginVertical: 30,
